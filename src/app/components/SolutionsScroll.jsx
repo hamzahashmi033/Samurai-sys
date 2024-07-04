095495b0da7aa6e903f203486cda8690ef9df171
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-07-03 22:42:46 -0700

    add solutions scrollbar component util file

--- src/app/components/SolutionsScroll.jsx
+++ src/app/components/SolutionsScroll.jsx
@@ -1,30 +1,6 @@
 import Image from "next/image";
 import React from "react";
-// import { dataSolutions } from "../utils/Solutions.constants";
-
-const dataSolutions = [
-  {
-    id: 1,
-    category: "Cybersecurity",
-    heading: "GRC Consultation & Implementation",
-    description:
-      "Investing in Governance, Risk, and Compliance (GRC) is critical to long-term growth, value, and sustainability. However, right-sizing your effort to meet regulations...",
-  },
-  {
-    id: 2,
-    category: "Digital Transformation",
-    heading: "Digital Led Strategic Transformation",
-    description:
-      "Investing in Governance, Risk, and Compliance (GRC) is critical to long-term growth, value, and sustainability. However, right-sizing your effort to meet regulations...",
-  },
-  {
-    id: 3,
-    category: "Cybersecurity",
-    heading: "GRC Consultation & Implementation",
-    description:
-      "Investing in Governance, Risk, and Compliance (GRC) is critical to long-term growth, value, and sustainability. However, right-sizing your effort to meet regulations...",
-  },
-];
+import { dataSolutions } from "../utils/Solutions.constants";
 
 const Solutions = () => {
   return (
