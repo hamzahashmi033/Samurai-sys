db2be3e0bccd494e6a3bd301e7145067fd62e039
hamzahashmi033 <hashmihamza846@gmail.com>
2024-07-04 21:28:58 +0500

    initial animations added

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,55 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
 import Client from "./components/Client";
 import Hero from "./components/Hero";
-import Nav from "./components/Nav";
+import HeroSecond from "./components/HeroSecond";
+
+import { motion, AnimatePresence } from 'framer-motion';
+import Solutions from "./components/SolutionsScroll";
+import MainFooter from "./components/MainFooter";
+
+const components = [Hero, HeroSecond, Solutions, Client, MainFooter];
 
 export default function Home() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const refs = useRef([]);
+
+  const handleScroll = () => {
+    for (let i = 0; i < refs.current.length; i++) {
+      const ref = refs.current[i];
+      if (ref && ref.getBoundingClientRect().top < window.innerHeight / 2) {
+        setCurrentIndex(i);
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div>
-      {/* <Nav /> */}
-      <Hero />
-      <Client />
-    </div>
+    <>
+      <div style={{
+        backgroundColor: 'black', height: '100vh',
+      }}>
+        <AnimatePresence>
+          {components.map((Component, index) => (
+            <motion.div
+              key={index}
+              ref={(el) => (refs.current[index] = el)}
+              initial={{ opacity: 0 }}
+
+              animate={{ opacity: currentIndex === index ? 1 : 0 }}
+              exit={{ opacity: 0.4 }}
+              transition={{ duration: 0.4 }}
+              viewport={{ once: true }}
+            >
+              <Component />
+            </motion.div>
+          ))}
+        </AnimatePresence>
+      </div>
+    </>
   );
 }
